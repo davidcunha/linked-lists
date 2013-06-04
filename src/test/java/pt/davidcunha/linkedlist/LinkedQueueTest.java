86961eea0f8c9a67e7e6fe822e8fb9a2a3651fc7
davidcunha <davidgoncalvescunha@gmail.com>
2013-06-04 22:04:16 +0100

    queue using a linked list pass tests

---
+++ src/test/java/pt/davidcunha/linkedlist/LinkedQueueTest.java
@@ -0,0 +1,20 @@
+package pt.davidcunha.linkedlist;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for linked queue implementation
+ */
+public class LinkedQueueTest {
+    
+        @Test
+    public void setup() {
+        LinkedQueue lQueue = new LinkedQueue();
+        lQueue.insert(10, 10.1);
+        lQueue.insert(20, 20.1);
+        assertEquals("{10,10.1}->{20,20.1}->null", lQueue.peek());
+        lQueue.remove();
+        assertEquals("{20,20.1}->null", lQueue.peek());
+    }
+}
